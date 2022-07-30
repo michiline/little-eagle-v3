f4923166caac5fa49a244e8af0c3107f99cec489
Mich Iline <mislav@pm.me>
2022-07-30 20:13:08 +0200

    fix import

--- components/Images/Images.tsx
+++ components/Images/Images.tsx
@@ -1,18 +1,10 @@
 import { ImageProps } from 'next/image'
-import { aboutLogo, aboutTripodImage, footerLogo, homeAbout, CamBirdWhiteLogoUrl } from 'utils/const'
+import { aboutLogo, aboutTripodImage, footerLogo, homeAbout } from 'utils/const'
 import { ImageBase } from './Base'
 
 export const FooterLogo = () => <ImageBase src={footerLogo} width={190} alt={'alt'} />
 
 type StyledImageProps = Pick<ImageProps, 'style' | 'className'>
-
-export const HomeAboutImage = ({ style }: StyledImageProps) => (
-  <ImageBase style={style} src={homeAbout} alt={'alt'} fullWidth={true} />
-)
-
-export const HomeLogo = ({ style }: StyledImageProps) => (
-  <ImageBase width={190} style={style} src={CamBirdWhiteLogoUrl} alt={'alt'} />
-)
 
 export const AboutLogo = ({ style, className }: StyledImageProps) => (
   <ImageBase width={191} style={style} src={aboutLogo} alt={'alt'} className={className} />
