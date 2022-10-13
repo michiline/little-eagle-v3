9b4ff305f63c0d79f89879d6446b6b077132e404
Mich Iline <mislav@pm.me>
2022-10-13 16:46:53 +0200

    fixed image optimization for client gallery

--- modules/Client/components/SquareImageGrid/SquareImageGrid.tsx
+++ modules/Client/components/SquareImageGrid/SquareImageGrid.tsx
@@ -6,18 +6,7 @@ const SquareImageGrid = ({ images, handleClick }) => {
   const theme = useTheme()
   return (
     <StyledSquareImageGrid>
-      {images.slice(0, 4).map((elem, index) => (
-        <ListItem key={index} onClick={() => handleClick(index)}>
-          <Image src={elem} alt={index} layout="fill" objectFit="cover" />
-        </ListItem>
-      ))}
-      {images.slice(0, 4).map((elem, index) => (
-        <ListItem key={index} onClick={() => handleClick(index)}>
-          <Image src={elem} alt={index} layout="fill" objectFit="cover" quality={30} />
-        </ListItem>
-      ))}
-
-      {images.slice(0, 4).map((elem, index) => (
+      {images.map((elem, index) => (
         <ListItem key={index} onClick={() => handleClick(index)}>
           <Image
             src={elem}
@@ -28,15 +17,14 @@ const SquareImageGrid = ({ images, handleClick }) => {
           />
         </ListItem>
       ))}
-      {images.slice(0, 4).map((elem, index) => (
-        <ListItem key={index} onClick={() => handleClick(index)}>
+      {images.slice(0, images.length % 4).map((elem, index) => (
+        <ListItem key={index} hidden={true}>
           <Image
             src={elem}
             alt={index}
             layout="fill"
             objectFit="cover"
             sizes={`(min-width: ${theme.breakpoints.lg}px) 25vw, 100vw`}
-            quality="30"
           />
         </ListItem>
       ))}
@@ -47,9 +35,3 @@ const SquareImageGrid = ({ images, handleClick }) => {
 export default SquareImageGrid
 
 // sizes={`(min-width: ${theme.breakpoints.lg}px) 25vw, 100vw`}
-
-// {images.slice(0, images.length % 4).map((elem, index) => (
-//   <ListItem key={index} hidden={true}>
-//     <Image src={elem} alt={index} layout="fill" objectFit="cover" quality={30} />
-//   </ListItem>
-// ))}
